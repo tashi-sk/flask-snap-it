bc2133c14ba7526552d01c3a57bcab3a9775c5a1
tashi-sk <tashi_sk@hotmail.com>
2021-06-17 01:12:50 +0000

    Create Upload template

--- app.py
+++ app.py
@@ -118,6 +118,11 @@ def community():
                            pagination=pagination)
 
 
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    return render_template("upload.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
