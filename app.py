4668d872b3a1ae0220f2435684b965541dfd5525
tashi-sk <tashi_sk@hotmail.com>
2021-06-17 12:40:09 +0000

    500 internal error handling functionality added

--- app.py
+++ app.py
@@ -288,6 +288,12 @@ def not_found_error(error):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_error(error):
+
+    return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
