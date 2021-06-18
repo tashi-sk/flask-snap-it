2ee131bf819e83083b8de7edd1554ea8a30fe784
tashi-sk <tashi_sk@hotmail.com>
2021-06-18 02:43:41 +0000

    Code tidied and commented out

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,21 @@
 // to show and hide edit/delete comments 
 $(document).ready(function () {
   $(".drop-down").click(function() {
-    $(this).parent().prev().toggle()
+    $(this).parent().prev().toggle();
     $(this).next().css('display', 'block');
     $(this).css('display', 'none');
   });
   $('.cross').click(function(){
-    $(this).parent().prev().toggle()
+    $(this).parent().prev().toggle();
     $(this).css('display', 'none');
-    $(this).prev().css('display','block')
-  })
-  updateYear()
+    $(this).prev().css('display','block');
+  });
+  updateYear();
 });
 
+// To insert current year on footer copyright year text
 var copyrightYear = document.getElementById('year');
 
 function updateYear(){
-    copyrightYear.innerText = new Date().getFullYear()
+    copyrightYear.innerText = new Date().getFullYear();
 }
